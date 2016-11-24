b8ca58c0586aa4155d2dd87e02d1ab8d3cf392eb
TravisMadill <TravisMadill@users.noreply.github.com>
2016-11-24 16:39:32 -0500

    Update update.php

--- update.php
+++ update.php
@@ -7,11 +7,13 @@ $playerlist = json_decode($playerlist, true);
 print_r($obj);
 echo "</pre>"; // */
 
-$sql = new mysqli("localhost", "root", "x9fN#DXy", "LeagueData");
+$sql = new mysqli("localhost", "root", "", "LeagueData");
 if($sql->connect_errno)
 	die("Connection to MySQL database failed: " . $sql->connect_error);
 $a = $sql->query("DELETE FROM PlayerStats;");
 $a = $sql->query("DELETE FROM Player;");
+if($sql->connect_errno)
+	die("Delete failed: " . $sql->connect_error);
 
 for($i = 1; $i <= 45; $i++){
 	$j = rand(0, count($playerlist['resultSets'][0]['rowSet']));
@@ -21,9 +23,9 @@ for($i = 1; $i <= 45; $i++){
 	$playerinfo = json_decode($playerinfo, true);
 	$fname = explode(", ", $playerlist['resultSets'][0]['rowSet'][$j][1])[1];
 	$lname = explode(", ", $playerlist['resultSets'][0]['rowSet'][$j][1])[0];
-	$hometown = $playerinfo['resultSets'][0]['rowSet'][0][7];
+	$college = $playerinfo['resultSets'][0]['rowSet'][0][7];
 	$position = $playerinfo['resultSets'][0]['rowSet'][0][14];
-	$query = 'INSERT INTO Player(TeamNo, Fname, Lname, College, Position) VALUES(' . intval(($i-1)/5+1) . ', \'' . $fname .  '\', \'' . $lname . '\', \'' . $hometown . '\', \'' .$position.'\');';
+	$query = 'INSERT INTO Player(TeamNo, Fname, Lname, Hometown, Position) VALUES(' . intval(($i-1)/5+1) . ', \'' . str_replace("'", "\\'", $fname) .  '\', \'' . str_replace("'", "\\'", $lname) . '\', \'' . str_replace("'", "\\'", $college) . '\', \'' .str_replace("'", "\\'", $position).'\');';
 	echo $query;
 	try{
 		$a = $sql->query($query);
