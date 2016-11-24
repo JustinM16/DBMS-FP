e256a53ad9782cd55bcb20eb7cc850c2b2847ebe
TravisMadill <TravisMadill@users.noreply.github.com>
2016-11-24 15:35:03 -0500

    Update update.php

--- update.php
+++ update.php
@@ -7,7 +7,7 @@ $playerlist = json_decode($playerlist, true);
 print_r($obj);
 echo "</pre>"; // */
 
-$sql = new mysqli("localhost", "root", "x9fN#DXy", "LeagueData");
+$sql = new mysqli("159.203.93.30", "root", "x9fN#DXy", "LeagueData");
 if($sql->connect_error)
 	die("Connection to MySQL database failed: " . self::$sqlConnection->connect_error);
 $sql->query("DELETE FROM PlayerStats; DELETE FROM Player;");
