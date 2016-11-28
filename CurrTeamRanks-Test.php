24c4bd696456c1e17e0b6d693f2e4c087ef53993
Varun-Gopikrishna <varun.gopikrishna@gmail.com>
2016-11-28 15:09:26 -0500

    Edited and Testing the Team Ranking Page

---
+++ CurrTeamRanks-Test.php
@@ -0,0 +1,116 @@
+<!DOCTYPE HTML>
+<!--
+	Minimaxing by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+        
+		<title>Check out the latest and updated Team Rankings!</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+        <style>
+        
+        table {
+            border-collapse: collapse;
+        }
+
+        table, th, td {
+            border: 1px solid black;
+            padding: 5px
+        
+        }
+            th{
+                font-weight:bold
+                
+            }
+        </style>
+	</head>
+	<body>
+		<div id="page-wrapper">
+			<div id="header-wrapper">
+				<div class="container">
+					<div class="row">
+						<div class="12u">
+
+							<header id="header">
+								<h1><img src="images/BBLogo-4.gif" style="float:left;width:70px;height:70px;" href="index.html" ><a href="index.html" id="logo">League-O-Matic 3000</a></h1>
+								<nav id="nav">
+									<a href="IntroPage.html">Introduction</a>
+									<a href="MakeTeamPage.html">Make A Team</a>
+									<a href="CurrTeamRanks.php" class="current-page-item">Current Team Rankings</a>
+									<a href="MatchSchedulePage.html">Match Schedule</a>
+								</nav>
+							</header>
+
+						</div>
+					</div>
+				</div>
+			</div>
+			<div id="main">
+				<div class="container">
+					<div class="row main-row">
+						<div class="8u 12u(mobile)">
+
+							<section class="left-content">
+                                
+<!--
+                                <table>
+                                    <tr>
+                                        <th>Ranking</th>
+                                        <th>Team Name</th>
+                                        <th>Wins</th>
+                                        <th>Losses</th>
+                                        <th>OtherCol</th>                 
+                                    </tr>
+                                    
+                                    
+                                </table>
+                                
+-->
+                                <?php
+									$sql = new mysqli("localhost", "root", "x9fN#DXy", "LeagueData");
+									if($sql->connect_errno)
+										die("Connection to MySQL database failed: " . $sql->connect_error);
+									$query = "SELECT TeamRank, TeamName, Wins, Losses FROM Standings, Team WHERE Standings.TeamNo = Team.TeamNo ORDER BY TeamRank ASC";
+
+									$result = $sql->query($query);
+
+									if ($result->num_rows > 0){
+                                        
+                                        echo "<table>";
+                                        echo "<tr><th>Ranking</th><th>Team Name</th><th>Wins</th><th>Losses</th><th>OtherCol</th></tr>";
+                                        
+                                        
+										while($row = $result->fetch_assoc()){
+											echo "<tr><th>". $row["TeamRank"] . "</th><th>" . $row["TeamName"] . "</th><th>" . $row["Wins"] . "</th><th>" . $row["Losses"]. "</tr>";
+										}
+									} else {
+										echo "0 results";
+									}
+									
+								?>
+				            </section>
+							<section class="right-content">
+	
+							</section>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/skel-viewport.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
