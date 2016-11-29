236a8ffae032ef2c817dc8c6ea8a580007fd4fe8
JustinM16 <Justin.Mendes16@gmail.com>
2016-11-28 20:42:48 -0500

    THIS TIME

--- ViewTeamPlayers.php
+++ ViewTeamPlayers.php
@@ -63,7 +63,7 @@
 										if($sql->connect_errno)
 											die("Connection to MySQL database failed: " . $sql->connect_error);
 									
-										$query = "SELECT Fname, Lname, College, Position, TotalPoints, TotalAssists, GamesPlayed FROM Player, Team, PlayerStats WHERE PlayerStats.PlayerID = Player.PlayerID AND Team.TeamName LIKE '$Team'";
+										$query = "SELECT Fname, Lname, College, Position, TotalPoints, TotalAssists, GamesPlayed FROM Player, Team, PlayerStats WHERE PlayerStats.PlayerID = Player.PlayerID AND Team.TeamName LIKE '"$Team"'";
 
 										$result = $sql->query($query);
 										if ($result->num_rows > 0){
