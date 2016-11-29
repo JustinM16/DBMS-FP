c1698a20850e236c59f876bf2f14959c6eb52c81
JustinM16 <Justin.Mendes16@gmail.com>
2016-11-28 20:29:25 -0500

    another thing

--- ViewTeamPlayers.php
+++ ViewTeamPlayers.php
@@ -59,7 +59,7 @@
 										if($sql->connect_errno)
 											die("Connection to MySQL database failed: " . $sql->connect_error);
 									
-										$query = "SELECT Fname, Lname, College, Position, TotalPoints, TotalAssists, GamesPlayed FROM Player, Team, PlayerStats WHERE PlayerStats.PlayerID = Player.PlayerID AND Team.TeamName = " $_GET['submit'];
+										$query = "SELECT Fname, Lname, College, Position, TotalPoints, TotalAssists, GamesPlayed FROM Player, Team, PlayerStats WHERE PlayerStats.PlayerID = Player.PlayerID AND Team.TeamName LIKE '" . mysql_escape_string($_GET['submit']) . "';";
 
 										$result = $sql->query($query);
 										if ($result->num_rows > 0){
