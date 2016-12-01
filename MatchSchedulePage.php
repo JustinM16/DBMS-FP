6c03cb3fb13a783bd9b577ec14e7b6364b3b4cc7
TravisMadill <TravisMadill@users.noreply.github.com>
2016-11-30 20:51:14 -0500

    Update MatchSchedulePage.php

--- MatchSchedulePage.php
+++ MatchSchedulePage.php
@@ -8,6 +8,7 @@
 	<head>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF8">
 		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
 		<link rel="stylesheet" href="assets/css/main.css" />
 		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
